75fe83ab5fd46456651f2dc48764cb7f92f1661a
drapt1 <110332491+drapt1@users.noreply.github.com>
2022-11-02 21:05:02 +0500

    Update lab 11.cpp

--- lab 11/lab 11.cpp
+++ lab 11/lab 11.cpp
@@ -1,90 +1,120 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cstdlib>
 #include <locale.h>
-
+#pragma warning(disable: 4996)
 using namespace std;
-
+ 
 struct Elem {
     int key;
     Elem* pnext;
+    static unsigned siz;
 };
-
-int first_elem(Elem** pbeg, Elem** pend, int d) {
+ 
+unsigned Elem::siz = 0;
+ 
+int first_elem(Elem* *pbeg, Elem* *pend, int d) {
     Elem* ph = new Elem;
     ph->key = d;
     ph->pnext = 0;
     *pbeg = ph;
     *pend = ph;
-
+    ++Elem::siz;
+ 
     return 0;
 }
-int add_elem(Elem** pend, int d) {
+int add_elem(Elem* *pend, int d) {
     Elem* ph = new Elem;
     ph->key = d;
     ph->pnext = 0;
     (*pend)->pnext = ph;
     *pend = ph;
-
+    ++Elem::siz;
+ 
     return 0;
 }
-int del_elem(Elem** pbeg, Elem** pend, int k) {
-    Elem* pk = 0;
-    Elem* ph = *pbeg;
-    if (!*pbeg)
+int del_elem(Elem* *pbeg, Elem* *pend, int k) {
+    Elem* tmp = *pbeg;
+    if (!*pbeg) {
         cout << "Ошибка: список пуст" << endl;
-    return 0;
-
+        return 0;
+    }
+ 
     if ((*pbeg)->key == k) {
-        if (*pbeg = *pend) {
+        if (*pbeg == *pend) {
             *pbeg = 0;
             *pend = 0;
-            delete pk;
+            delete tmp;
+            --Elem::siz;
         }
         else {
             *pbeg = (*pbeg)->pnext;
-            delete ph;
+            delete tmp;
+            --Elem::siz;
         }
     }
     else {
-        while ((ph->pnext) && (ph->pnext->key != k))
-            ph = ph->pnext;
-        pk = ph->pnext;
-        if (pk) {
-            ph->pnext = pk->pnext;
-            delete pk;
+        while (tmp->pnext && tmp->pnext->key != k) {
+            tmp = tmp->pnext;
         }
-        else
+        if (tmp->pnext) {
+            Elem* tmp1 = tmp->pnext->pnext;
+            delete tmp->pnext;
+            tmp->pnext = tmp1;
+            --Elem::siz;
+            if (!tmp->pnext) {
+                *pend = tmp;
+            }
+        }
+        else{
             return 0;
+        }
     }
     return 1;
 }
-void work(Elem** pbeg, int z) {}
-void print_elem(Elem* pbeg) {
+void del_list(Elem *pbeg) {
+    if (pbeg != NULL) {
+        del_list(pbeg->pnext);
+        delete pbeg;
+        --Elem::siz;
+ 
+    }
+}
+void del(Elem* *pbeg, Elem* *pend, unsigned const n)
+{
+    Elem** tmp = pbeg;
+    Elem* prev = nullptr;
+    for (int rem = Elem::siz; rem > n; --rem)
+    {
+        prev = *tmp;
+        tmp = &(*tmp)->pnext;
+    }
+    del_list(*tmp);
+    *pend = prev;
+    *tmp = nullptr;
+}
+void print_elem(Elem *pbeg) {
     Elem* ph = pbeg;
+    cout << "siz: " << Elem::siz << '\n';
     while (ph) {
         cout << ph->key << ' ';
         ph = ph->pnext;
     }
     cout << endl;
 }
-void del_list(Elem* pbeg) {
-    if (pbeg != NULL) {
-        del_list(pbeg->pnext);
-        delete pbeg;
-    }
-}
 int main() {
     setlocale(LC_ALL, "Russian");
     Elem* pbeg = 0;
     Elem* pend = 0;
     int d;
-    int g = 0;
     int a;
     int k;
+    int g = 0;
     int z;
-    char s;
     char ch;
+    bool exit = false;
     do {
-        cout << " Нажмите 1 - добавить элемент/создать, 2 - удалить, 3 - просмотр списка: " << endl;
+        system("cls");
+        cout << "1 - добавить элемент/создать\n2 - удалить один\n3 - удалить несколько\n4 - просмотр\n5 - выход" << endl;
         cin >> ch;
         switch (ch) {
         case '1':
@@ -98,43 +128,40 @@ int main() {
                 cout << "Введите новый элемент списка: " << endl;
                 cin >> a;
                 add_elem(&pend, a);
-                cout << "Список всех элементов" << endl;
-                print_elem(pbeg);
-                g = 1;
                 break;
             }
+            g = 1;
         case '2':
             cout << "Введите значение элемента для удаления: " << endl;
             cin >> k;
             if (del_elem(&pbeg, &pend, k) == 0) {
                 cout << "Элемент для удаления не найден" << endl;
-                cout << "Список всех элементов" << endl;
-                print_elem(pbeg);
-            }
-            else {
-                del_elem(&pbeg, &pend, k);
-                cout << "Список всех элементов" << endl;
-                print_elem(pbeg);
             }
             break;
         case '3':
-            if (g == 1)
-            {
-            cout << "Список всех элементов" << endl;
+            cout << "Сколько хотите удалить элементов с конца: " << endl;
+            cin >> z;
+            del(&pbeg, &pend, z);
+            break;
+        case '4':
+            if (g == 1) {
+                cout << "Список всех элементов" << endl;
                 print_elem(pbeg);
-                break;
             }
             else
-                cout << "Список пуст" <<endl;
+                cout << "Список пуст" << endl;
+            break;
+        case '5':
+            exit = true;
+            break;
         }
         _flushall;
-        cout << "Повторить (1-да/2-нет)" << endl;
-        cin >> s;
-    } while (s == '1');
-    system("Pause");
+        system("Pause");
+    } while (!exit);
     if (pbeg != NULL) {
         del_list(pbeg->pnext);
         delete pbeg;
+        Elem::siz = 0;
     }
     return 0;
 }
